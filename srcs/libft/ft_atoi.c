64b375eae47a87049bf778af0439b6e32da3fdb6
jpham005 <jpham005@naver.com>
2022-02-14 06:15:26 +0900

    atoi

--- srcs/libft/ft_atoi.c
+++ srcs/libft/ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaham <jaham@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jaham <jaham@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 17:12:04 by jaham             #+#    #+#             */
-/*   Updated: 2022/02/13 21:00:21 by jaham            ###   ########.fr       */
+/*   Updated: 2022/02/14 06:15:07 by jaham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	ft_atoi(const char *str)
 {
 	int				sign;
 	unsigned long	ret;
-	unsigned long	temp;
 
 	while (*str == ' ' || (9 <= *str && *str <= 13))
 		str++;
@@ -25,10 +24,12 @@ int	ft_atoi(const char *str)
 	ret = 0;
 	while ('0' <= *str && *str <= '9')
 	{
-		temp = ret * 10 + (*str - '0');
-		if (temp > (unsigned long) LONG_MAX + (sign < 0) || ret > temp)
-			return (-1 + (sign < 0));
-		ret = temp;
+		if (ret > (unsigned long) LONG_MAX / 10 + (sign < 0))
+			return ((sign < 0) - 1);
+		ret *= 10;
+		if (ret > (unsigned long) LONG_MAX - *str + '0' + (sign < 0))
+			return ((sign < 0) - 1);
+		ret += (*str - '0');
 		str++;
 	}
 	return (ret * sign);
