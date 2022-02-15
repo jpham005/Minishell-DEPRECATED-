194d52dff9e9e6d0c7a112336e0dba68311dc971
함재원 <jaham@hamjaewon-ui-MacBookAir.local>
2022-02-15 17:53:10 +0900

    cd

--- srcs/built_in/cd.c
+++ srcs/built_in/cd.c
@@ -3,29 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaham <jaham@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jaham <jaham@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 15:41:42 by jaham             #+#    #+#             */
-/*   Updated: 2022/02/14 21:43:31 by jaham            ###   ########.fr       */
+/*   Updated: 2022/02/15 17:06:07 by jaham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "built_in.h"
 #include "terminal.h"
 
+int	test_path(const char *path_value, const char *str)
+{
+	char	**path;
+
+	path = ft_split(path_value, ':');
+}
+
+int	move_by_home(t_context *context)
+{
+	t_envp_list	*home;
+
+	home = find_list_by_key(context->envp, "HOME");
+	if (!home)
+	{
+		ft_putstr_fd(HOME_NOT_SET_ERR_MESSAGE, 2);
+		return (1);
+	}
+
+}
+
 int	cd(t_context *context, const char **argv)
 {
 	t_envp_list	*home;
 
 	if (!argv[1])
-	{
-		home = find_list_by_key(context->envp, "HOME");
-		if (!home || )
-		{
-			ft_putstr_fd(HOME_NOT_SET_ERR_MESSAGE, 2);
-			return (1);
-		}
-	}
+		move_by_home(context);
 }
 
 /*
@@ -40,4 +53,6 @@ cd ../
 cd - : display oldpwd+\n, move to oldpwd
 oldpwd
 pwd
+Not a directory
+bash: cd: asdf: Permission denied
 */
